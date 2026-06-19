042c3d27457cbc29fd4d236c84b9cf697fc4f10c
FoziaAkhtar <fozia.shah0001@gmail.com>
2026-06-19 08:20:08 -0400

    Initial Commit

---
+++ IOSApp3 Watch App/IOSApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp3App.swift
+//  IOSApp3 Watch App
+//
+//  Created by Fozia on 2026-06-19.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp3_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
