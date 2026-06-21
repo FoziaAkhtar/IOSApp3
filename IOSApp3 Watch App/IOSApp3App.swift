1fb48a74b3857b48c7c7bfd07b9ae681a53905f0
FoziaAkhtar <fozia.shah0001@gmail.com>
2026-06-21 18:28:22 -0400

    updated

--- IOSApp3 Watch App/IOSApp3App.swift
+++ IOSApp3 Watch App/IOSApp3App.swift
@@ -1,14 +1,10 @@
 
-//
-//  IOSApp3App.swift
-//  IOSApp3 Watch App
-//
-//  Created by Fozia on 2026-06-19.
-//
-//  This is the entry point of the application.
-//  The app starts by loading the HomeView screen.
-//
 
+// ==========================================
+// Purpose:
+// Entry point for the Watch App.
+// Loads the main screen when the app launches.
+// ===========================================
 import SwiftUI
 
 @main
@@ -18,8 +14,9 @@ struct IOSApp3_Watch_AppApp: App {
 
         WindowGroup {
 
-            // Opens the main menu screen when the app launches.
+            // Root screen of the watch app
             HomeView()
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
